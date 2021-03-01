850d0d6267070777b9afe2d336c587ae82e592d2
sorinneculae <sorin.neculae@gmail.com>
2021-03-01 14:10:33 +0200

    Implement mediapipe

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import './style.css';
-import PianoPNG from './piano.png';
-import aModule from './a-module';
 
-import * as tf from "@tensorflow/tfjs";
-import * as handpose from "@tensorflow-models/handpose";
 import { drawHand } from "./utilities";
-
 
 const video = document.getElementById('video');
 const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext("2d");
+let canvasWidth;
+let canvasHeight;
 
 function startVideo() {
   navigator.getUserMedia(
@@ -22,32 +20,39 @@ function startVideo() {
     stream => video.srcObject = stream,
     err => console.error(err)
   );
-}
-
-function startDetection() {
-  const runHandpose = async () => {
-    const net = await handpose.load();
-    detect(net);
-  };
-
-  const detect = async (net) => {
-    requestAnimationFrame(() => detect(net));
-    // Check data is available
-    if (video.readyState === 4) {
-      // Set canvas height and width
-      canvas.width = video.offsetWidth;
-      canvas.height = video.offsetHeight;
-
-      // Make Detections
-      const hand = await net.estimateHands(video);
-      const ctx = canvas.getContext("2d");
-      if (hand.length) {
-        drawHand(hand, ctx);
-      }
-    }
-  };
-  runHandpose();
+  const playPromise = document.querySelector('video').play();
+  if (playPromise) {
+    playPromise.then(response => {
+      const videoRatio = video.offsetWidth / video.offsetHeight;
+      canvas.width = 780;
+      canvas.height = canvas.width / videoRatio;
+      canvasWidth = canvas.width;
+      canvasHeight = canvas.height;
+      detect();
+    })
+    .catch(error => { console.error(error) });
+  }
 }
 
 startVideo();
-startDetection();
+
+function onResults(results) {
+  drawHand(results, ctx, canvasWidth, canvasHeight);
+}
+
+const holistic = new Holistic({locateFile: (file) => {
+  return `https://cdn.jsdelivr.net/npm/@mediapipe/holistic@0.1/${file}`;
+}});
+holistic.setOptions({
+  selfieMode: false,
+  upperBodyOnly: true,
+  smoothLandmarks: true,
+  minDetectionConfidence: 0.5,
+  minTrackingConfidence: 0.5
+});
+holistic.onResults(onResults);
+
+const detect = async () => {
+  await holistic.send({ image: video });
+  requestAnimationFrame(detect);
+};
--- src/utilities.js
+++ src/utilities.js
@@ -1,86 +1,32 @@
 // Points for fingers
-const fingerJoints = {
-  thumb: [0, 1, 2, 3, 4],
-  indexFinger: [0, 5, 6, 7, 8],
-  middleFinger: [0, 9, 10, 11, 12],
-  ringFinger: [0, 13, 14, 15, 16],
-  pinky: [0, 17, 18, 19, 20],
-};
-
-// Infinity Gauntlet Style
-const style = {
-  0: { color: "yellow", size: 15 },
-  1: { color: "gold", size: 6 },
-  2: { color: "green", size: 10 },
-  3: { color: "gold", size: 6 },
-  4: { color: "gold", size: 6 },
-  5: { color: "purple", size: 10 },
-  6: { color: "gold", size: 6 },
-  7: { color: "gold", size: 6 },
-  8: { color: "gold", size: 6 },
-  9: { color: "blue", size: 10 },
-  10: { color: "gold", size: 6 },
-  11: { color: "gold", size: 6 },
-  12: { color: "gold", size: 6 },
-  13: { color: "red", size: 10 },
-  14: { color: "gold", size: 6 },
-  15: { color: "gold", size: 6 },
-  16: { color: "gold", size: 6 },
-  17: { color: "orange", size: 10 },
-  18: { color: "gold", size: 6 },
-  19: { color: "gold", size: 6 },
-  20: { color: "gold", size: 6 },
-};
+const points = [4, 8, 12, 16, 20];
 
 // Drawing function
-export const drawHand = (predictions, ctx) => {
-  // Check if we have predictions
-  if (predictions.length > 0) {
-    // Loop through each prediction
-    predictions.forEach((prediction) => {
-      // Grab landmarks
-      const landmarks = prediction.landmarks;
+export const drawHand = (results, ctx, w, h) => {
+  // Loop through each prediction
+  ctx.save();
+  ctx.clearRect(0, 0, w, h);
+  ctx.drawImage(results.image, 0, 0, w, h);
 
-      // Loop through fingers
-      for (let j = 0; j < Object.keys(fingerJoints).length; j++) {
-        let finger = Object.keys(fingerJoints)[j];
-        //  Loop through pairs of joints
-        for (let k = 0; k < fingerJoints[finger].length - 1; k++) {
-          // Get pairs of joints
-          const firstJointIndex = fingerJoints[finger][k];
-          const secondJointIndex = fingerJoints[finger][k + 1];
+  const rH = results?.rightHandLandmarks || [];
+  const lH = results?.leftHandLandmarks || [];
+  const hands = [rH, lH];
 
-          // Draw path
-          ctx.beginPath();
-          ctx.moveTo(
-            Math.round(landmarks[firstJointIndex][0]),
-            Math.round(landmarks[firstJointIndex][1])
-          );
-          ctx.lineTo(
-            Math.round(landmarks[secondJointIndex][0]),
-            Math.round(landmarks[secondJointIndex][1])
-          );
-          ctx.strokeStyle = "plum";
-          ctx.lineWidth = 4;
-          ctx.stroke();
-        }
-      }
-
-      // Loop through landmarks and draw em
-      for (let i = 0; i < landmarks.length; i++) {
-        // Get x point
-        const x = Math.round(landmarks[i][0]);
+  hands.forEach(hand => {
+    // Loop through fingers
+    if (hand.length) {
+      points.forEach(point => {
+        const x = hand[point].x * w;
         // Get y point
-        const y = Math.round(landmarks[i][1]);
-        console.log(x, y);
+        const y = hand[point].y * h;
         // Start drawing
         ctx.beginPath();
-        ctx.arc(x, y, style[i]["size"], 0, 3 * Math.PI);
-
+        ctx.arc(x, y, 6, 0, 3 * Math.PI);
         // Set line color
-        ctx.fillStyle = style[i]["color"];
+        ctx.fillStyle = 'gold';
         ctx.fill();
-      }
-    });
-  }
+      });
+    }
+  });
+  ctx.restore();
 };
